dcd5fdcfdeac2761a90795305c52ed54d1d307c1
vv3rd <lyudskoy@gmail.com>
2025-06-14 01:01:08 +0500

    added portals, might not need 'em

--- hosts/zenbook/configuration.nix
+++ hosts/zenbook/configuration.nix
@@ -19,6 +19,7 @@ let
         module-essentials
         module-user
         module-audio
+        module-portals
         # module_desktop-Plasma
         module-desktop-Niri
         module-browser-Firefox
@@ -177,6 +178,36 @@ let
       programs.niri = {
         enable = true;
         # package = inputs.niri.packages.${system}.niri;
+      };
+    };
+
+  module-portals =
+    { pkgs, ... }:
+    {
+      environment.systemPackages = with pkgs; [
+        xdg-desktop-portal-gtk
+        xdg-desktop-portal-termfilechooser
+        xdg-desktop-portal-wlr
+      ];
+      xdg.portal = {
+        enable = true;
+        config = {
+          niri = {
+            default = [
+              "gnome"
+              "gtk"
+            ];
+            "org.freedesktop.impl.portal.Access" = "gtk";
+            "org.freedesktop.impl.portal.FileChooser" = "termfilechooser";
+            "org.freedesktop.impl.portal.ScreenCast" = "wlr";
+            "org.freedesktop.impl.portal.Secret" = "gnome-keyring";
+          };
+        };
+        extraPortals = with pkgs; [
+          xdg-desktop-portal-gtk
+          xdg-desktop-portal-termfilechooser
+          xdg-desktop-portal-wlr
+        ];
       };
     };
 
