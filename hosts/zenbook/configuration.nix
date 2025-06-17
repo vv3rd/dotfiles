7c5db6a7184943ee848c20a9184ed3f955cc42e1
vv3rd <lyudskoy@gmail.com>
2025-06-17 22:38:35 +0500

    finally working portals

--- hosts/zenbook/configuration.nix
+++ hosts/zenbook/configuration.nix
@@ -202,28 +202,19 @@ let
     {
       environment.systemPackages = with pkgs; [
         xdg-desktop-portal-gtk
-        xdg-desktop-portal-termfilechooser
         xdg-desktop-portal-wlr
       ];
       xdg.portal = {
         enable = true;
         config = {
           niri = {
-            default = [
-              "gnome"
-              "gtk"
-            ];
-            "org.freedesktop.impl.portal.Access" = "gtk";
-            "org.freedesktop.impl.portal.FileChooser" = "termfilechooser";
+            default = [ "gtk" ];
             "org.freedesktop.impl.portal.ScreenCast" = "wlr";
-            "org.freedesktop.impl.portal.Screenshot" = "wlr";
-            "org.freedesktop.impl.portal.Secret" = "gnome-keyring";
           };
         };
-        wlr.enable = true;
         extraPortals = with pkgs; [
           xdg-desktop-portal-gtk
-          xdg-desktop-portal-termfilechooser
+          xdg-desktop-portal-wlr
         ];
       };
     };
