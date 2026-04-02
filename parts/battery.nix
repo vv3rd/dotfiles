beb37fa8df969e9c8e1a42a3253bb070b5d5f08d
vv3rd <lyudskoy@gmail.com>
2026-04-03 00:39:08 +0500

    Disabled tlp. Fixed fans

--- parts/battery.nix
+++ parts/battery.nix
@@ -3,7 +3,7 @@
   include.nixos.${inputs.personal.host}.module = {
     services.upower.enable = true;
     services.tlp = {
-      enable = true;
+      enable = false;
       settings = {
         START_CHARGE_THRESH_BAT0 = 40; # 40 and below it starts to charge
         STOP_CHARGE_THRESH_BAT0 = 80; # 80 and above it stops charging
